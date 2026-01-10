0185255670b75e7338907e3bbe8ccb3b592a2dbe
Yogesh Chaudhary <8106yogi@gmail.com>
2026-01-09 23:50:25 -0600

    Symmetry in Syntax: C++ Butterfly Pattern Logic

---
+++ ButterFlyPattern.cpp
@@ -0,0 +1,76 @@
+//Inout: 4
+//Output:
+//*      *
+//**    **
+//***  ***
+//********
+//********
+//***  ***
+//**    **
+//*      *
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+int n=0;
+
+cout << "Enter the number" << endl;
+
+cin >> n;
+
+for(int i=0;i<n;i++){
+
+for(int j=0;j<i+1;j++){
+
+cout << "*";
+
+}
+
+for(int j=0;j<2*(n-i-1);j++){
+
+cout << " ";
+
+}
+
+for(int j=0; j<i+1;j++)
+
+{
+
+cout << "*";
+
+}
+
+cout <<endl;
+
+}
+
+for(int i=0;i<n;i++){
+
+for(int j=0; j<n-i;j++){
+
+cout << "*";
+
+}
+
+for(int j=0; j<2*i; j++){
+
+cout <<" ";
+
+}
+
+for(int j=0; j<n-i;j++){
+
+cout << "*";
+
+}
+
+cout << endl;
+
+}
+
+return 0;
+
+}
